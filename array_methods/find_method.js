a270ed61dbc196de9daffa33500d52045e9de4bf
zahrabateninia <zahrabatenin@gmail.com>
2023-11-07 16:48:21 +0330

    Find the user with id one in an array of objects.

---
+++ array_methods/find_method.js
@@ -0,0 +1,9 @@
+let users = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Pete"},
+    {id: 3, name: "Mary"}
+  ];
+  
+  let user = users.find(item => item.id == 1);
+  
+  console.log(user.name); // John
