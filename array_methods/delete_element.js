adc1c4bb37669e8af5ca32727ba146e360abf13f
zahrabateninia <zahrabatenin@gmail.com>
2023-11-07 15:23:47 +0330

    It should be splice, not slice.

--- array_methods/delete_element.js
+++ array_methods/delete_element.js
@@ -6,9 +6,7 @@ delete arr[1]; // remove "go"
 
 alert( arr[1] ); // undefined
 
-// now arr = ["I",  , "home"];
 alert( arr.length ); // 3
 
-arr.slice(1,1) // from index one, remove one element.
-// now arr is ["I", "home"]
+arr.splice(1,1); // from index one, remove one element.
 
