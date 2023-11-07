e2c1b2a339ec42d6cd61ad588ee3c3cb0c20609f
zahrabateninia <zahrabatenin@gmail.com>
2023-11-07 16:59:13 +0330

    Filter the objects that their id is less than 3

--- array_methods/find_method.js
+++ array_methods/find_method.js
@@ -1,4 +1,4 @@
-let users = [
+ let users = [
     {id: 1, name: "John"},
     {id: 2, name: "Pete"},
     {id: 3, name: "Mary"}
@@ -7,3 +7,7 @@ let users = [
   let user = users.find(item => item.id == 1);
   
   console.log(user.name); // John
+
+  let someUsers = users.filter(item => item.id < 3);
+
+  console.log(someUsers); 
