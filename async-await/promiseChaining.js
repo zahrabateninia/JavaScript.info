7ed92a68b0a99a281fda688b8e93fa0f5bbc5741
zahrabateninia <zahrabatenin@gmail.com>
2024-02-09 11:46:22 +0330

    Fetch a url, if response.status was 200, retrun response.json otherwise throw Error

---
+++ async-await/promiseChaining.js
@@ -0,0 +1,14 @@
+function loadJson(url){
+    return fetch(url)
+    .then(response =>{
+        if(response.status === 200){
+            return response.json
+        }else{
+            throw new Error(response.status)
+        }
+    })
+}
+
+
+loadJson('https://javascript.info/no-such-user.json')
+  .catch(alert); // Error: 404
