a13bdf045509e9e532e8fa07fc1093562560a4cb
zahrabateninia <zahrabatenin@gmail.com>
2024-02-09 11:58:10 +0330

    Rewrite the code with async/await instead of then/catch

--- async-await/promiseChaining.js
+++ async-await/promiseChaining.js
@@ -1,12 +1,19 @@
-function loadJson(url){
-    return fetch(url)
-    .then(response =>{
-        if(response.status === 200){
-            return response.json
-        }else{
-            throw new Error(response.status)
-        }
-    })
+async function loadJson(url){
+    // return fetch(url)
+    // .then(response =>{
+    //     if(response.status === 200){
+    //         return response.json;
+    //     }else{
+    //         throw new Error(response.status);
+    //     }
+    // })
+    let response = await fetch(url)
+    if(response.status === 200){
+        let json = await response.json
+        return json 
+    }
+
+    throw new Error(response.status)
 }
 
 
