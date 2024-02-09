8fad35145026413ee520ff066fd4c390e238f51b
zahrabateninia <zahrabatenin@gmail.com>
2024-02-09 12:29:05 +0330

    Replace .catch with try..catch and add async/await where needed

--- async-await/demoGithubUser.js
+++ async-await/demoGithubUser.js
@@ -6,34 +6,61 @@ class HttpError extends Error {
     }
   }
   
-  function loadJson(url) {
-    return fetch(url)
-      .then(response => {
-        if (response.status == 200) {
-          return response.json();
-        } else {
-          throw new HttpError(response);
-        }
-      });
+  async function loadJson(url) {
+    // return fetch(url)
+    //   .then(response => {
+    //     if (response.status == 200) {
+    //       return response.json();
+    //     } else {
+    //       throw new HttpError(response);
+    //     }
+    //   });
+    let response = await fetch(url)
+    if(response.status == 200){
+        return response.json;
+    }
+
+    throw new HttpError(response);
   }
   
   // Ask for a user name until github returns a valid user
-  function demoGithubUser() {
+  async function demoGithubUser() {
     let name = prompt("Enter a name?", "iliakan");
   
-    return loadJson(`https://api.github.com/users/${name}`)
-      .then(user => {
-        alert(`Full name: ${user.name}.`);
-        return user;
-      })
-      .catch(err => {
+    // return loadJson(`https://api.github.com/users/${name}`)
+    //   .then(user => {
+    //     alert(`Full name: ${user.name}.`);
+    //     return user;
+    //   })
+    let user;
+    while(true) {
+      let name = prompt("Enter a name?", "iliakan");
+  
+      try {
+        user = await loadJson(`https://api.github.com/users/${name}`);
+        break; // no error, exit loop
+      } catch(err) {
         if (err instanceof HttpError && err.response.status == 404) {
+          // loop continues after the alert
           alert("No such user, please reenter.");
-          return demoGithubUser();
         } else {
+          // unknown error, rethrow
           throw err;
         }
-      });
+      }
+    } 
+     //   .catch(err => {
+    //     if (err instanceof HttpError && err.response.status == 404) {
+    //       alert("No such user, please reenter.");
+    //       return demoGithubUser();
+    //     } else {
+    //       throw err;
+    //     }
+    //   });
+
+  alert(`Full name: ${user.name}.`);
+  return user;
+
   }
   
   demoGithubUser();
